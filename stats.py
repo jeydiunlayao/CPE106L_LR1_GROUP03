5f67b77e7182e20a2f6d80c4de9f2f785acfed78
Josel Dominic Unlayao <113328857+jeydiunlayao@users.noreply.github.com>
2022-10-06 19:47:43 +0800

    Add files via upload

---
+++ stats.py
@@ -0,0 +1,9 @@
+import statistics
+
+mylist =  [32, 25, 51, 65, 5, 2, 21]
+
+print("Mode: ",statistics.mode(mylist))
+
+print("Median: ",statistics.median(mylist))
+
+print("Mean: ",statistics.mean(mylist))
